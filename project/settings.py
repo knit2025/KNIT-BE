7e18388a6ea90d5b6b67bebb2d24c540ab2a134c
junh5ng <wnsghd1130@dgu.ac.kr>
2025-11-11 16:58:15 +0900

    adminqa/customqa/main apps settings.py 등록

--- project/settings.py
+++ project/settings.py
@@ -38,9 +38,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
     'rest_framework',
     #'rest_framework_simplejwt',
-    'accounts'
+    'accounts',
+    'missions',
+    'adminqa',
+    'customqa',
+    'main',
 ]
 
 AUTH_USER_MODEL = 'accounts.User' 
