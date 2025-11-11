df1db7db9b3363d7f0917e01e69ce085daa1fecc
junh5ng <wnsghd1130@dgu.ac.kr>
2025-11-12 01:25:07 +0900

    Revert "Merge pull request #2 from knit2025/junhong"
    This reverts commit c18002d006e7d8cddd856f458f50623cd87d7197, reversing
    changes made to 5255291c58fa9f19fa62f0c32604978ff14165d7.

--- project/settings.py
+++ project/settings.py
@@ -38,17 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
     'rest_framework',
-    #'rest_framework_simplejwt',
-    'accounts',
-    #'missions',
-    #'adminqa',
-    #'customqa',
-    #'main',
+    'movie',
 ]
-
-AUTH_USER_MODEL = 'accounts.User' 
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
@@ -57,17 +49,6 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
-from datetime import timedelta
-
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
-    'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': False,
 }
 
 MIDDLEWARE = [
