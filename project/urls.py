2a402c320bc4e5efb998265ea158e69b52c2bb7c
junh5ng <wnsghd1130@dgu.ac.kr>
2025-11-11 11:04:23 +0900

    project/urls.py 라우팅 연결

--- project/urls.py
+++ project/urls.py
@@ -18,7 +18,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include('accounts.urls'))
+    # path("auth/", include('accounts.urls')),
+
+    # JWT 인증 관련 endpoint
+    path("api/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
+    # accounts 앱 라우팅 (회원가입, 로그인 등)
+    path("api/accounts/", include("accounts.urls")),
+
 ]
