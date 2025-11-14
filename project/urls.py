8bc1cf1efebdbcb8dcb72e56f99640a7087dbb89
onowoey <cg10046@naver.com>
2025-11-14 13:41:51 +0900

    Fix: 이미지 경로 명시

--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -29,3 +31,5 @@ urlpatterns = [
     path("memory/",include("memory.urls")),
     path("home/", include("main.urls")),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
